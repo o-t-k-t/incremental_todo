98686e942f2808e8e77445fb909ee7fc5d8afe2d
helium <wetcondition@gmail.com>
2018-11-05 14:44:16 +0900

    日本(東京)へのタイムゾーン設定

--- config/application.rb
+++ config/application.rb
@@ -8,8 +8,12 @@ module IncrementalTodo
   class Application < Rails::Application
     config.load_defaults 5.2
 
+    # 日本国内向け設定
     config.i18n.default_locale = :ja
     Faker::Config.locale = :ja
+
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
 
     config.generators do |g|
       g.assets false

1b79cdf4a87f70d43a2a6345511e3bc7c97930c7
helium <wetcondition@gmail.com>
2018-11-06 17:54:49 +0900

    タスクモデルに期限情報を追加

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,5 +2,8 @@ class Task < ApplicationRecord
   validates :name, presence: true, length: { maximum: 255 }
   validates :description, length: { maximum: 2000 }
 
+  validates_datetime :deadline, after: -> { DateTime.current }, allow_blank: true
+
   scope :recent, -> { order('created_at desc') }
+  scope :deadline_asc, -> { order('deadline asc') }
 end

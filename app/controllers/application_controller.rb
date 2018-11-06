e9caef8561a43f0b8b07c019e5494f979ae9b4e8
helium <wetcondition@gmail.com>
2018-11-06 15:36:41 +0900

    Production環境でのBasic認証設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,3 @@
 class ApplicationController < ActionController::Base
+  http_basic_authenticate_with name: ENV['BASIC_AUTH_USERNAME'], password: ENV['BASIC_AUTH_PASSWORD'] if Rails.env == 'production'
 end

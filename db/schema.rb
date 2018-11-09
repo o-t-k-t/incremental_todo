214716f650b6f064e98afc890788e28781b6de48
helium <wetcondition@gmail.com>
2018-11-09 11:34:27 +0900

    タスク優先度の設定

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_08_090923) do
+ActiveRecord::Schema.define(version: 2018_11_09_020512) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2018_11_08_090923) do
     t.datetime "updated_at", null: false
     t.datetime "deadline"
     t.string "status", default: "not_started", null: false
+    t.integer "priority", default: 2, null: false
     t.index ["name"], name: "index_tasks_on_name"
     t.index ["status"], name: "index_tasks_on_status"
   end
--- spec/factories/task.rb
+++ spec/factories/task.rb
@@ -3,17 +3,21 @@ FactoryBot.define do
     sequence(:name) { |n| "task-#{n}" }
     description '何かする'
   end
+
   trait :homework_by_12 do
     name '論文を書く'
     deadline DateTime.new(2018, 11, 12, 13, 14, 15)
+    priority 3
   end
 
   trait :shopping_by_13 do
     name 'パンを買う'
     deadline DateTime.new(2018, 11, 13, 13, 14, 15)
+    priority 1
   end
 
   trait :cleanup do
     name '掃除する'
+    priority 2
   end
 end

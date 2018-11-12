1f8d7b6aebba8c8a5618a8dead1d5ac63080eb6c
helium <wetcondition@gmail.com>
2018-11-12 19:07:40 +0900

    ユーザモデル・タスクとの関連作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_09_020512) do
+ActiveRecord::Schema.define(version: 2018_11_12_080325) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,8 +23,19 @@ ActiveRecord::Schema.define(version: 2018_11_09_020512) do
     t.datetime "deadline"
     t.string "status", default: "not_started", null: false
     t.integer "priority", default: 2, null: false
+    t.bigint "user_id"
     t.index ["name"], name: "index_tasks_on_name"
     t.index ["status"], name: "index_tasks_on_status"
+    t.index ["user_id"], name: "index_tasks_on_user_id"
   end
 
+  create_table "users", force: :cascade do |t|
+    t.string "name", default: "名なしさん", null: false
+    t.string "email", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+  end
+
+  add_foreign_key "tasks", "users"
 end

9cc0fd19245c83fcc959433318d32707c06c7b1e
helium <wetcondition@gmail.com>
2018-11-17 14:17:35 +0900

    管理者権限の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,10 +9,23 @@ class User < ApplicationRecord
   validates :password, presence: true, length: { in: 6..20 }
   validates :password_digest, presence: true
 
+  before_destroy :requere_administrator_existance
+
   scope :id_order, -> { order(:id) }
   scope :with_task, -> { left_joins(:tasks) }
 
   def self.count_by_id_and_name
     group('users.id', 'users.name').count('tasks.id')
+  end
+
+  private
+
+  def requere_administrator_existance
+    if where(admin: false).count.zero?
+      true
+    else
+      errors.add(:admin, 'を持つユーザは少なくとも1人登録する必要があります')
+      false
+    end
   end
 end

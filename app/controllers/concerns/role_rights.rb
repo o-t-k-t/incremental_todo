d13092042fcef5fe7fbec584c827ef8309d06035
helium <wetcondition@gmail.com>
2018-11-20 11:50:06 +0900

    カスタム権限エラーページを作成

--- app/controllers/concerns/role_rights.rb
+++ app/controllers/concerns/role_rights.rb
@@ -3,7 +3,17 @@ module RoleRights
 
   class PermissionError < StandardError; end
 
+  included do
+    rescue_from PermissionError, with: :render_405 unless Rails.env == 'development'
+  end
+
   def require_admin_authority
     raise PermissionError, 'login permission denied' unless current_user&.admin
+  end
+
+  private
+
+  def render_405(_exception)
+    render file: "#{Rails.root}/public/405.html", layout: false, status: 405
   end
 end

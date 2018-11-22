6c68cd4c2cd153592e9b5c5fc9c7213cfd7f646e
helium <wetcondition@gmail.com>
2018-11-22 16:33:44 +0900

    ユーザーページを単一リソースに変更(他ユーザー情報へのアクセス防止)

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
   root to: 'tasks#index'
   resources :tasks
-  resources :users, only: %i[show new create destroy]
+  resource :user, only: %i[show new create destroy]
   resources :sessions, only: %i[new create destroy]
-  resources :users, only: %i[new create destroy]
 
   namespace :admin do
     root to: 'users#index'

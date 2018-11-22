6d6c57c2fcc124af25465261ced837833a2bf32c
helium <wetcondition@gmail.com>
2018-11-22 17:47:55 +0900

    リベース後テストフィードバック

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_19_025728) do
+ActiveRecord::Schema.define(version: 2018_11_22_074335) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"

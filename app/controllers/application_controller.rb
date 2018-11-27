0767c7d01a26f786151201238229ee3e28df67a6
helium <wetcondition@gmail.com>
2018-11-27 16:50:41 +0900

    ログイン時のタスクアラーム更新

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,5 @@ class ApplicationController < ActionController::Base
   http_basic_authenticate_with name: ENV['BASIC_AUTH_USERNAME'], password: ENV['BASIC_AUTH_PASSWORD'] if Rails.env == 'production'
 
   before_action :require_logged_in
-  before_action -> { current_user&.update_delayed_tasks_alarm }
+  before_action -> { current_user&.periodical_alarm_update }
 end

ca9cf0f0ad205dc702c2a9e45534305c7c3fbece
helium <wetcondition@gmail.com>
2018-12-06 16:50:50 +0900

    TerraformによるHeroku環境作成

---
+++ heroku.tf
@@ -0,0 +1,18 @@
+variable "heroku_email" {}
+variable "heroku_api_key" {}
+
+# Configure the Heroku provider
+provider "heroku" {
+  email   = "${var.heroku_email}"
+  api_key = "${var.heroku_api_key}"
+}
+
+# Create a new application
+resource "heroku_app" "incremental-todo" {
+  name   = "incremental-todo"
+  region = "us"
+}
+resource "heroku_addon" "redis" {
+  app  = "${heroku_app.incremental-todo.name}"
+  plan = "heroku-redis:hobby-dev"
+}

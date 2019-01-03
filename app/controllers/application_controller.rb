50d2b37ed7a425e88f6da953ccc8596d526a3ec1
o-t-k-t <wetcondition@gmail.com>
2019-01-04 01:10:14 +0900

    ポートフォリオ向けにBasic認証解除

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,6 @@ class ApplicationController < ActionController::Base
   include SessionControl
   include Banken
 
-  http_basic_authenticate_with name: ENV['BASIC_AUTH_USERNAME'], password: ENV['BASIC_AUTH_PASSWORD'] if Rails.env == 'production'
-
   before_action :require_logged_in
   before_action -> { current_user&.periodical_alarm_update }
 

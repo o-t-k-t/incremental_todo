67d88d82f706a95096c7783b70061837d16c3493
o-t-k-t <wetcondition@gmail.com>
2019-01-15 21:19:34 +0900

    グループ管理機能のテスト追加

--- spec/factories/groups.rb
+++ spec/factories/groups.rb
@@ -3,4 +3,11 @@ FactoryBot.define do
     name { 'ユーザー友の会' }
     description { 'みなさんのタスクを見せ合いましょう' }
   end
+
+  trait :other_users do
+    after(:create) do |group|
+      user = create(:user)
+      Membership.create_with_group!(user, group)
+    end
+  end
 end

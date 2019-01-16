2b6ddc9de58aee8d8f08ea4c443cd0e3ae2f68ad
o-t-k-t <wetcondition@gmail.com>
2019-01-16 19:35:51 +0900

    TerraformバックエンドにGSCを使用開始

--- heroku.tf
+++ heroku.tf
@@ -1,6 +1,13 @@
 variable "heroku_email" {}
 variable "heroku_api_key" {}
 
+terraform {
+  backend "gcs" {
+    bucket  = "complimentary-terraform-state"
+    prefix  = "incremental-todo"
+  }
+}
+
 # Configure the Heroku provider
 provider "heroku" {
   email   = "${var.heroku_email}"
@@ -12,6 +19,7 @@ resource "heroku_app" "incremental-todo" {
   name   = "incremental-todo"
   region = "us"
 }
+
 resource "heroku_addon" "redis" {
   app  = "${heroku_app.incremental-todo.name}"
   plan = "heroku-redis:hobby-dev"
